fd4dd29d2cffcf1a3a38badcbf395ce572a5684f
scott <scott@SORAN>
2024-03-04 17:04:21 +0330

    ContactUsCreate

--- Application/DTOs/ContactUses/ContactUsCreateDTO.cs
+++ Application/DTOs/ContactUses/ContactUsCreateDTO.cs
@@ -1,12 +1,17 @@
-﻿namespace Application.DTOs.ContactUs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.ContactUs
 {
     public class ContactUsCreateDTO
     {
+        [MaxLength(20, ErrorMessage = "شماره تلفن اجباری است طول 20 کاراکتر است ")]
         public required string Phone { get; set; }
         public string? Fax { get; set; }
         public string? Twitter { get; set; }
         public string? WhatsApp { get; set; }
+        [MaxLength(500, ErrorMessage = "آدرس  اجباری است طول 500 کاراکتر است ")]
         public required string Address { get; set; }
+        [MaxLength(500, ErrorMessage = "آدرس  اجباری است طول 500 کاراکتر است ")]
         public required string Email { get; set; }
         public float Lan { get; set; }
         public float Lat { get; set; }
