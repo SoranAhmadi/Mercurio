d1f3525b36ed848c6842e0e9112eaa7df9081083
scott <scott@SORAN>
2024-03-05 12:46:29 +0330

    Created IOverViewService

---
+++ Application/IServices/IOverViewService.cs
@@ -0,0 +1,13 @@
+﻿using Application.DTOs.ContactUs;
+using Application.DTOs.OVerViews;
+
+namespace Application.IServices
+{
+    public interface IOverViewService
+    {
+        Task<OverViewDTO> Get();
+        Task<int> Create(OverViewCreateDTO overViewCreateDTO);
+        Task Update(OverViewUpdateDTO overViewUpdateDTO);
+
+    }
+}
