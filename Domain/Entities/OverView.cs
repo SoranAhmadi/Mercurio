dfbf8fb10957c687405ba570fde93017553ff6a8
scott <scott@SORAN>
2024-03-05 12:29:11 +0330

    Created OverViewConfiguration

---
+++ Domain/Entities/OverView.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities
+{
+    public class OverView:Entity
+    {
+        public required string Image { get; set; }
+        public required string Description { get; set; }
+    }
+}
---
+++ Infrastructure/Configuration/OverViewConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration
+{
+    public class OverViewConfiguration : IEntityTypeConfiguration<OverView>
+    {
+        public void Configure(EntityTypeBuilder<OverView> builder)
+        {
+        }
+    }
+}
