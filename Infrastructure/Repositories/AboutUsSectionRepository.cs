93eaec3c4cbf92f11ab28d4dbe5449ca9f7d0864
scott <scott@SORAN>
2024-03-05 16:03:46 +0330

    Created AboutUsSectionRepository, IAboutUsSectionRepository

---
+++ Infrastructure/Repositories/AboutUsSectionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.DbContexts;
+
+namespace Infrastructure.Repositories
+{
+    public class AboutUsSectionRepository:Repository<AboutUsSection>, IAboutUsSectionRepository
+    {
+        public AboutUsSectionRepository(DbContextMericurio context) : base(context)
+        {
+
+        }
+    }
+}
