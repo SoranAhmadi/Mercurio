b1890c813a7774b0e3b33d465f4b496feb563fc5
scott <scott@SORAN>
2024-03-05 12:14:24 +0330

    Created Message Error

--- Application/DTOs/WhyUses/WhyUsCreateDTO.cs
+++ Application/DTOs/WhyUses/WhyUsCreateDTO.cs
@@ -1,9 +1,13 @@
-﻿namespace Application.DTOs.WhyUses
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs.WhyUses
 {
     public class WhyUsCreateDTO
     {
         public int Priority { get; set; }
+        [MaxLength(200, ErrorMessage = "عنوان اجباری است و حداکثر طول آن 200 کارکتر است")]
         public required string Title { get; set; }
+        [MaxLength(2000, ErrorMessage = "توضیحات اجباری است و حداکثر طول آن 2000 کارکتر می باشد.")]
         public required string Description { get; set; }
     }
 }
