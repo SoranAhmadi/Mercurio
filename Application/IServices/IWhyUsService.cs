303a2dc6020cb6c3ce7c2367f2deff506ec9b98d
scott <scott@SORAN>
2024-03-05 11:04:19 +0330

    Created IWhyUsService

--- Application/IServices/IWhyUsService.cs
+++ Application/IServices/IWhyUsService.cs
@@ -1,7 +1,15 @@
-﻿namespace Application.IServices
+﻿using Application.DTOs.Service;
+using Application.DTOs.WhyUses;
+
+namespace Application.IServices
 {
     public interface IWhyUsService
     {
+        Task<IEnumerable<WhyUsDTO>> GetAll();
+        Task<int> Create(WhyUsCreateDTO whyUsCreateDTO);
+        Task Delete(WhyUsDeleteDTO delete);
+        Task<WhyUsUpdateDTO> GetById(WhyUsByIdDTO whyUsByIdDTO);
+        Task Update(WhyUsUpdateDTO whyUsUpdateDTO);
 
     }
 }
