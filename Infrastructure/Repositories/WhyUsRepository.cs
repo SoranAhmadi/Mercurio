392d31eba1d335fa2910fdbad7c8137524b42020
scott <scott@SORAN>
2024-03-05 09:47:13 +0330

    Created WhyUsRepository

---
+++ Infrastructure/Repositories/WhyUsRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.DbContexts;
+
+namespace Infrastructure.Repositories
+{
+    public class WhyUsRepository:Repository<WhyUs>, IWhyUsRepository
+    {
+        public WhyUsRepository(DbContextMericurio context) : base(context)
+        {
+        }
+    }
+}
