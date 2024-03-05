b47df343478d0fdd977fd02c365d41f810190d1a
scott <scott@SORAN>
2024-03-05 09:29:12 +0330

    WhyUs entiry

---
+++ Domain/Entities/WhyUs.cs
@@ -0,0 +1,10 @@
+﻿namespace Domain.Entities
+{
+    public class WhyUs:Entity
+    {
+        public int Priority { get; set; }
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+
+    }
+}
