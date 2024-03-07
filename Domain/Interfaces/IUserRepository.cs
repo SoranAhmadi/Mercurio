941180bf4c4511344363efdf8cecc0a637fe1306
scott <scott@SORAN>
2024-03-07 10:54:48 +0330

    Created IUserRepository and UserRepository

---
+++ Domain/Interfaces/IUserRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces
+{
+    public interface IUserRepository:IRepository<User>
+    {
+       new Task<int> Insert(User user);
+    }
+}
---
+++ Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infrastructure.DbContexts;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository:Repository<User>, IUserRepository
+    {
+        public UserRepository(DbContextMericurio context) : base(context)
+        {
+        }
+    }
+}
