596ec5151beead5e6dc4681e2244112040f0c876
scott <scott@SORAN>
2024-03-07 10:22:56 +0330

    Create  UserConfiguration also applay configuration to the database

--- Infrastructure/Configuration/UserConfiguration.cs
+++ Infrastructure/Configuration/UserConfiguration.cs
@@ -1,6 +1,17 @@
-﻿namespace Infrastructure.Configuration
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Configuration
 {
-    public class UserConfiguration
+    public class UserConfiguration : IEntityTypeConfiguration<User>
     {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(s => s.FirstName).HasMaxLength(100);
+            builder.Property(s => s.LastName).HasMaxLength(100);
+            builder.Property(s => s.UserName).HasMaxLength(50);
+            builder.Property(s => s.Password).HasMaxLength(500);
+        }
     }
 }
